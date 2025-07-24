e2f441fcd4f784223c0639523e5b62113d3be687
Jose Luiz Rodrigues <zeluizr@me.com>
2025-07-24 14:44:06 -0400

    refactor: remover configurações desnecessárias do Prettier e ajustar formatação de código

--- node/index.ts
+++ node/index.ts
@@ -6,7 +6,7 @@ import { searchmindaiMiddleware } from './middlewares/searchmindai'
 const TIMEOUT_MS = 5000
 const memoryCache = new LRUCache<string, never>({ max: 5000 })
 
-metrics.trackCache('status', memoryCache)
+metrics.trackCache("status", memoryCache)
 
 const clients: ClientsConfig<Clients> = {
 	implementation: Clients,
