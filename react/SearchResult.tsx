dcb20f67f4d89fed25a5002fc2799ddc6f114345
Jose Luiz Rodrigues <zeluizr@me.com>
2025-07-24 14:22:45 -0400

    refactor: corrigir texto do título e ajustar formatação do componente de resultados de busca

--- react/SearchResult.tsx
+++ react/SearchResult.tsx
@@ -1,17 +1,19 @@
-import classNames from "classnames";
-import { useCssHandles } from "vtex.css-handles";
-import "./styles/styles.css";
+import classNames from 'classnames'
+import { useCssHandles } from 'vtex.css-handles'
+import './styles/styles.css'
 
-const CSS_HANDLES = ["searchResultContainer"] as const;
+const CSS_HANDLES = ['searchResultContainer'] as const
 
 function SearchResult() {
-  const { handles } = useCssHandles(CSS_HANDLES);
+  const { handles } = useCssHandles(CSS_HANDLES)
 
   return (
-    <section className={classNames(handles.searchResultContainer, "relative w-100")}>
-      <h1>Result</h1>
+    <section
+      className={classNames(handles.searchResultContainer, 'relative w-100')}
+    >
+      <h1>Result da página</h1>
     </section>
-  );
+  )
 }
 
-export default SearchResult;
+export default SearchResult
